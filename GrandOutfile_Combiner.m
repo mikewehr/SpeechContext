29f4be81d1c4c12513fe685004bce49744635217
tai-heinzerling <32080484+tai-heinzerling@users.noreply.github.com>
2021-07-14 16:50:37 -0700

    Update GrandOutfile_Combiner.m

--- GrandOutfile_Combiner.m
+++ GrandOutfile_Combiner.m
@@ -1,10 +1,10 @@
 clear all;
-masterdir = "D:\lab\djmaus\Data\sfm\2021-01-18_11-55-16_mouse-0295" %set master directory
-targetdir = "F:\Data\sfm\0295"
+masterdir = "D:\lab\djmaus\Data\sfm\2021-01-18_11-55-16_mouse-0295"; %set master directory
+targetdir = "F:\Data\sfm";
 
 cd(masterdir)
 load("dirs.mat")
-outfilecelllist = dir('outPSTH*.mat')
+outfilecelllist = dir('outPSTH*.mat');
 for i = 1:length(outfilecelllist)
     for j = 1:24 %1:length(dirs) %or 162 (total outfile number per mouse)?
         %             k = j-12;
@@ -12,8 +12,9 @@ for i = 1:length(outfilecelllist)
         %             tempsplit = strsplit(outfilecelllist(i).name, 'ch')
         %             tempsplit = strsplit(tempsplit{2}),
         %             load('outPSTH_ch%dc%d.mat', Out.tetrode, Out.cell) %make unique values so only the same outfile is selected each pass
-        if ~isempty(outfilecelllist(i).spiketimes)
-            singleoutfilelist{j} = strcat(dirs{j},'\', outfilecelllist(i).name);
+        load(string(outfilecelllist(1).name));
+        if ~isempty(out.spiketimes) == 1
+            singleoutfilelist{j} = strcat(dirs{j},'\', outfilecelllist(i).name); % I think there is a way to do this faster (set up outfiles so that the entire thing doesn't have to be loaded) but not sure, this does work though - SFM 7/14/21
         end
     end
         
