f118561f090cee6dd55152398c42e88db2c35a45
tai-heinzerling <32080484+tai-heinzerling@users.noreply.github.com>
2021-07-15 10:49:19 -0700

    Update GrandOutfile_Combiner.m

--- GrandOutfile_Combiner.m
+++ GrandOutfile_Combiner.m
@@ -6,21 +6,21 @@ cd(masterdir)
 load("dirs.mat")
 outfilecelllist = dir('outPSTH*.mat');
 for i = 1:length(outfilecelllist)
+    k = 0;
     for j = 1:24 %1:length(dirs) %or 162 (total outfile number per mouse)?
         %             k = j-12;
-        cd (dirs{j}) %or 1:12 and 13:24 (number of dirs that may need splitting)?
-        %             tempsplit = strsplit(outfilecelllist(i).name, 'ch')
-        %             tempsplit = strsplit(tempsplit{2}),
-        %             load('outPSTH_ch%dc%d.mat', Out.tetrode, Out.cell) %make unique values so only the same outfile is selected each pass
+        cd (dirs{j}) %or 1:12 and 13:24 (number of dirs that may need splitting)
         load(string(outfilecelllist(i).name));
         if ~isempty(out.spiketimes) == 1
-            singleoutfilelist{j} = strcat(dirs{j},'\', outfilecelllist(i).name); % I think there is a way to do this faster (set up outfiles so that the entire thing doesn't have to be loaded) but not sure, this does work though - SFM 7/14/21
+            k = k+1;
+            singleoutfilelist{k} = strcat(dirs{j},'\', outfilecelllist(i).name); % I think there is a way to do this faster (set up outfiles so that the entire thing doesn't have to be loaded) but not sure, this does work though - SFM 7/14/21
         end  %Maybe use append() here instead? - SFM 7/14/21
+        clear out
     end
         
         Outfile_Combiner(singleoutfilelist, targetdir)
-        %save(combinedoutfilename, 'out', '-v7.3') %override line 426 in Outfile_Combiner?
         i
+        clear singleoutfilelist
 end
 
 fprintf("Outfiles for this selection have been combined and saved to")
