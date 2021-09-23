9b2b402820226c581a253f7f28a4299ba167cc39
tai-heinzerling <sammehan6@gmail.com>
2021-09-23 15:41:27 -0700

    Update SFMMachineLearning.m

--- SFMMachineLearning.m
+++ SFMMachineLearning.m
@@ -12,24 +12,25 @@ end
 
 % % Preprocessing of Raster Data
 
-rasterdir = 'F:\Data\sfm\Synthetic Test Data\Group6';         % Enter dir with rastered data if skipping the step from above - SFM 9/8/21
+rasterdir = 'F:\Data\sfm\RasterFiles';                        % Enter dir with rastered data if skipping the step from above - SFM 9/8/21
 cd(rasterdir)
 rasterlist = dir('*.mat');
 preprocess_switch = 1;
 decompression_switch = 0;
 real_data = 1;                                                % Binary switch on whether to organize synthetic data, won't work with real data - SFM 9/13/21
 
-if preprocess_switch == 1
-    if real_data == 0
-        for i = 1:length(rasterlist)                          % Let's put everything in order just in case, but only for synth data! - SFM 9/3/21
-            presplit = strsplit(rasterlist(i).name, '_');
-            presplit2 = strsplit(presplit{5}, 'c');
-            dirindex = str2num(presplit2{3}); 
-            fixeddir(dirindex) = rasterlist(i);
-        end                             
-        rasterlist = fixeddir;
+
+if real_data == 0
+    for i = 1:length(rasterlist)                              % Let's put everything in order just in case, but only for synth data! - SFM 9/3/21
+        presplit = strsplit(rasterlist(i).name, '_');
+        presplit2 = strsplit(presplit{5}, 'c');
+        dirindex = str2num(presplit2{3});
+        fixeddir(dirindex) = rasterlist(i);
     end
+    rasterlist = fixeddir;
+end
     
+if preprocess_switch == 1    
     if decompression_switch == 1
         for i = 1:length(rasterlist)                          % Decompress any sparse coding scheme - SFM 9/8/21
             load(rasterlist(i).name);
@@ -85,12 +86,12 @@ toc
 xlim = -181.8672;                                               % Shouldn't ever change, if so use round(out.xlimits(1), 4) - SFM 9/8/21
 samprate = raster_site_info.samprate;                           % Also shouldn't change, but just in case we will get it from the raster/out data - SFM 9/8/21
 start_time = 175 - xlim;                                  
-end_time = 365 - xlim;
+end_time = 370 - xlim;                                          % Use 247.5 for only the consonant, 370 for full phoneme - SFM 9/23/21
 start_time_samp = round((start_time/1000) * samprate);
 end_time_samp = round((end_time/1000) * samprate);
 hertzconv = round((1 / ((end_time - start_time) / 1000)), 2);   % Factor to multiple spike counts by to convert to Hertz - SFM 9/9/21
 hertzconv_switch = 0;                                           % Switch on whether to convery spike counts to Hz - SFM 9/10/21
-min_reps = 100;                                                  % Set minimum number of repetitions each cell needs to have to be included - SFM 9/14/21
+min_reps = 100;                                                 % Set minimum number of repetitions each cell needs to have to be included - SFM 9/14/21
 min_indices = min_reps * length(uniquestimuli);
 
 list_of_stims_to_use = uniquestims(1:30);
@@ -161,14 +162,13 @@ if manual_cell_curation_switch == 1
     end
 %     selectedrasterlist = sort(selectedrasterlist.name, 'ascend');
 %     unselectedrasterlist = sort(unselectedrasterlist.name, 'ascend');
+    rasterlist = unselectedrasterlist;                            % Quick and dirty way to make this new table - SFM 9/16/21
 else
 end
 
-rasterlist = selectedrasterlist;                                % Quick and dirty way to make this new table - SFM 9/16/21
-
 for i = 1:length(rasterlist)                                    % Construct the data table from the raster data - SFM 9/8/21
     if ~isempty(setdiff(i, exclude_cells))
-        clear raster_data I raster_labels
+        clear raster_data I raster_labels raster_size
         load(rasterlist(i).name);
         if ~exist('raster_data', 'var')
             raster_data = zeros(raster_size);
@@ -221,12 +221,13 @@ end
 if split_switch == 1
     datatable_supervised = table(datatable_train);
     datatable_unsupervised = table(datatable_test);
-%    Stim = {'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA'}';
-    datatable_supervised = addvars(datatable_unsupervised, Stim);
+%    true_stims = {'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA'}';
+    datatable_supervised = addvars(datatable_unsupervised, true_stims);
 else
     datatable_unsupervised = table(datatable);
-    Stim = {'IBA', 'UDA', 'UDA', 'IBA', 'BA', 'DA', 'DA', 'BA', 'UBA', 'IDA', 'UBA', 'DA', 'DA', 'UDA', 'IBA', 'DA', 'IDA', 'BA', 'UBA', 'UBA', 'IDA', 'UDA', 'UDA', 'UBA', 'IDA', 'IBA', 'UBA', 'IBA', 'UBA', 'IDA', 'IBA', 'DA', 'BA'}';
-    datatable_supervised = addvars(datatable_unsupervised, Stim);
+%     true_stims = {'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'DA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA', 'BA'}';
+%     true_stims = {'IBA', 'UDA', 'UDA', 'IBA', 'BA', 'DA', 'DA', 'BA', 'UBA', 'IDA', 'UBA', 'DA', 'DA', 'UDA', 'IBA', 'DA', 'IDA', 'BA', 'UBA', 'UBA', 'IDA', 'UDA', 'UDA', 'UBA', 'IDA', 'IBA', 'UBA', 'IBA', 'UBA', 'IDA', 'IBA', 'DA', 'BA'}';
+%     datatable_supervised = addvars(datatable_unsupervised, true_stims);
 end
 
 groupname = strsplit(rasterdir, '\');
@@ -236,21 +237,24 @@ savedir = 'F:\Data\sfm\DataTables';
 cd(savedir);
 numtables = length(dir('*.mat'));
 save_switch = 1;
+cells_used = length(rasterlist) - length(list_of_cells_excluded);
+% full_trial_switch = 1;
+% average_switch = 1;
 
 if save_switch == 1
     if strcmp(datatype, 'Synthetic Test Data')
-        savename_supervised = strcat('Synth', groupname, 'Supervised_', num2str(numtables + 1));
-        savename_unsupervised = strcat('Synth', groupname, 'Unsupervised_', num2str(numtables + 1));
+%         savename_supervised = strcat('Synth', groupname, 'Supervised_', num2str(cells_used), 'Cells_', num2str(numtables + 1));
+        savename_unsupervised = strcat('Synth', groupname, 'Unsupervised_', num2str(cells_used), 'Cells_', num2str(numtables + 1));
     else
-        savename_supervised = strcat('ExperimentalDataSupervised_', num2str(min_reps), 'MinReps_', num2str(length(list_of_stims_to_use)), 'LabelsUsed_', num2str(numtables + 1));
-%         savename_unsupervised = strcat('ExperimentalDataUnsupervised_', num2str(min_reps), 'MinReps_', num2str(length(list_of_stims_to_use)), 'LabelsUsed_', num2str(numtables + 1));
+%         savename_supervised = strcat('ExperimentalDataSupervised_', num2str(cells_used), 'Cells_', num2str(min_reps), 'MinReps_', num2str(length(list_of_stims_to_use)), 'LabelsUsed_', num2str(numtables + 1));
+        savename_unsupervised = strcat('ExperimentalDataUnsupervised_', num2str(cells_used), 'Cells_', num2str(min_reps), 'MinReps_', num2str(length(list_of_stims_to_use)), 'LabelsUsed_', num2str(numtables + 1));
     end
-    save(savename_supervised, 'datatable_supervised', 'list_of_cells_excluded'); 
-%     save(savename_unsupervised, 'datatable_unsupervised', 'list_of_cells_excluded');
+%     save(savename_supervised, 'datatable_supervised', 'list_of_cells_excluded'); 
+    save(savename_unsupervised, 'datatable_unsupervised', 'list_of_cells_excluded');
 end
 
-% modeldir = 'F:\Data\sfm\Machine Learning Models';
-% cd(modeldir);
+modeldir = 'F:\Data\sfm\Machine Learning Models';
+cd(modeldir);
 
 quick_results_switch = 0;
 if quick_results_switch == 1
