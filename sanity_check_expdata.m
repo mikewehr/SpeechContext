34001fba12aa3197030be76d0e77898cb0e3bf28
samfind <70771925+samfind@users.noreply.github.com>
2021-11-22 15:34:09 -0800

    tweaks
    made new script that is basically the same but tailored for experimental data

--- sanity_check_expdata.m
+++ sanity_check_expdata.m
@@ -2,16 +2,15 @@
 
 clear
 tic
-cd('/Users/sammehan/Documents/Wehr Lab/SpeechContext2021/Data') % Set directory
+cd('/Users/sammehan/Documents/Wehr Lab/SpeechContext2021/Data')             % Set directory
 
 %%% Settings
-group = 'Group12';                                                           % What subdirectory in here?
+group = 'ExpData';                                                          % What subdirectory in here?
 plot_switch = 1;                                                            % Do you want all of the diagnostic plots? (neurometric curve plots not included)
-no_context_switch = 1;                                                      % Do you want all trials or just BA-DA?
-fit_model_switch = 1;                                                       % Do you need to open Classification Learner and fit a model?
+context_switch = 1;                                                         % Do you want all trials or just BA-DA?
+fit_model_switch = 0;                                                       % Do you need to open Classification Learner and fit a model?
 
-cd(group)
-if ~exist('GroupDataTable.mat', 'file')
+if ~exist('ExpDataTable.mat', 'file')
     d = dir('outPSTH*.mat');
     for i = 1:length(d)
         fprintf('\n%d/%d', i, length(d))
@@ -20,17 +19,17 @@ if ~exist('GroupDataTable.mat', 'file')
         data(i).M1OFF = out.out.M1OFF;
         data(i).mM1OFF = out.out.mM1OFF;
     end
-    save('GroupDataTable', 'data');
+    save('ExpDataTable', 'data');
     nreps = max(out.out.nreps(:));
 else
-    load('GroupDataTable.mat');
-    nreps = 40;
+    load('ExpDataTable.mat');
+    nreps = 100;
 end
 
 % trial-averaged
+start = 190;
+stop = 350;
 if ~exist('CellsByStimDatatable.mat', 'file')
-    start = 190;
-    stop = 350;
     for j = 1:length(data)
         mM1OFF = data(j).mM1OFF(:,2,2,1);
         for k = 1:length(mM1OFF)
@@ -51,7 +50,7 @@ if plot_switch == 1
     colormap jet
     xlabel('stimulus')
     ylabel('cell')
-    title([group, 'trial-averaged'])
+    title([group, ' trial-averaged'])
 
     figure
     plot(cellsbystim_datatable(1,1:30), cellsbystim_datatable(25,1:30), 'o')
@@ -70,27 +69,35 @@ end
 %%%
 % Single trials
 
+iExclude = 0;
 if ~exist('TotalTrials&MTrials.mat', 'file')
     TotalTrialsByStim = [];
     for j = 1:length(data)
         M1OFF = data(j).M1OFF(:,2,2,:);
-        for k = 1:size(M1OFF, 1)
-            for rep = 1:size(M1OFF, 4)
-                spiketimes = M1OFF(k, 1, 1, rep).spiketimes;
-                spikecount = length(find(spiketimes >= start & spiketimes <= stop));
-                sc(k, rep) = spikecount;
+        if size(M1OFF, 4) >= 100
+            for k = 1:size(M1OFF, 1)
+                for rep = 1:nreps
+                    spiketimes = M1OFF(k, 1, 1, rep).spiketimes;
+                    spikecount = length(find(spiketimes >= start & spiketimes <= stop));
+                    sc(k, rep) = spikecount;
+                end
             end
+        else
+            iExclude = iExclude + 1;
+            list_of_cells_excluded(iExclude) = j;
         end
-        scsorted = sc([1 3 4 5 6 7 8 9 10 2 11 13 14 15 16 17 18 19 20 12 21 23 24 25 26 27 28 29 30 22], :);
-        Mtrials(j,:,:) = scsorted;
-        % Mtrials is cells x stimulus x rep
-        TotalTrialsByStim = [TotalTrialsByStim scsorted];
+        try
+            scsorted = sc([1 3 4 5 6 7 8 9 10 2 11 13 14 15 16 17 18 19 20 12 21 23 24 25 26 27 28 29 30 22], :);
+            Mtrials(j,:,:) = scsorted;                                      % Mtrials is cells x stimulus x rep
+            TotalTrialsByStim = [TotalTrialsByStim scsorted];
+        catch
+        end
     end
     save('TotalTrials&MTrials.mat', 'TotalTrialsByStim', 'Mtrials');
 else
     load('TotalTrials&MTrials.mat');
 end
-if no_context_switch == 1
+if context_switch == 0
     TotalTrialsByStim = TotalTrialsByStim(1:10, :)';
 else
     TotalTrialsByStim = TotalTrialsByStim';
@@ -100,14 +107,14 @@ if plot_switch == 1
     imagesc(TotalTrialsByStim)
     xlabel('stimulus')
     ylabel('cells and trials')
-    title([group, 'single trials'])
+    title([group, ' single trials'])
 end
 
 if ~exist('TrialAveragedExemplars.mat', 'file')
     clear response CellsTrialAveragedExemplars
     CellsTrialAveragedExemplars = [];
     k = 0;
-    for i = [1 10] % BA and DA only
+    for i = [1 10]                                                          % BA and DA only
         for j = 1:nreps
             k = k + 1;
             StimID2(k) = i;
@@ -123,52 +130,73 @@ if plot_switch == 1
     imagesc(CellsTrialAveragedExemplars)
     xlabel('stimuli and trials')
     ylabel('cells')
-    title([group, 'single trials (CellsTrialAveragedExemplars)'])
+    title([group, ' single trials (CellsTrialAveragedExemplars)'])
 end
-% CellsTrialAveragedExemplars is cells x (stim * reps) and only has stimuli 1 and 10 (BA and DA)
-% CellsTrialAveragedExemplars is designed to be input for classification learner
+                                                                            % CellsTrialAveragedExemplars is cells x (stim * reps) and only has stimuli 1 and 10 (BA and DA)
+                                                                            % CellsTrialAveragedExemplars is designed to be input for classification learner
 
+stims = [1:30];                                                             % Set stims for constructing table if needed
 if ~exist('CellsTrialAveragedAllBADA.mat', 'file')
-    clear response CellsTrialAveragedBADA
-    CellsTrialAveragedAllBADA = [];
+    clear response CellsTrialAveragedAllBADA CellsTrialAveragedAllStims
+    if context_switch == 0
+        CellsTrialAveragedAllBADA = [];
+    elseif context_switch == 1
+        CellsTrialAveragedAllStims = [];
+    end
     k = 0;
-    for i = [1:30] % All BA-DA
+    for i = stims                                                           % All BA-DA
         for j = 1:nreps
             k = k + 1;
             StimID3(k) = i;
-            CellsTrialAveragedAllBADA(:, k) = Mtrials(:, i, j);
+            if context_switch == 0
+                CellsTrialAveragedAllBADA(:, k) = Mtrials(:, i, j);
+            elseif context_switch == 1
+                CellsTrialAveragedAllStims(:, k) = Mtrials(:, i, j);
+            end
         end
     end
-% CellsTrialAveragedBADA is cells x (stim * reps)
-% CellsTrialAveragedBADA is designed to be input for classification learner
+                                                                            % CellsTrialAveragedBADA is cells x (stim * reps)
+                                                                            % CellsTrialAveragedBADA is designed to be input for classification learner
     coerce_switch = 1;
     if coerce_switch == 1
         StimID3_Coerce = zeros(length(StimID3), 1);
         for i = 1:length(StimID3)
             if StimID3(i) == 1 || StimID3(i) == 10
                 StimID3_Coerce(i) = StimID3(i);
-            elseif StimID3(i) == 2 || StimID3(i) == 3 || StimID3(i) == 4 || StimID3(i) == 5
+            elseif StimID3(i) == 2 || StimID3(i) == 3 || StimID3(i) == 4 || StimID3(i) == 5 || StimID3(i) == 11 || StimID3(i) == 12 || StimID3(i) == 13 || StimID3(i) == 14 || StimID3(i) == 15 || StimID3(i) == 21 || StimID3(i) == 22 || StimID3(i) == 23 || StimID3(i) == 24 || StimID3(i) == 25
                 StimID3_Coerce(i) = 1;
-            elseif StimID3(i) == 6 || StimID3(i) == 7 || StimID3(i) == 8 || StimID3(i) == 9
+            elseif StimID3(i) == 6 || StimID3(i) == 7 || StimID3(i) == 8 || StimID3(i) == 9 || StimID3(i) == 16 || StimID3(i) == 17 || StimID3(i) == 18 || StimID3(i) == 19 || StimID3(i) == 20 || StimID3(i) == 26 || StimID3(i) == 27 || StimID3(i) == 28 || StimID3(i) == 29 || StimID3(i) == 30
                 StimID3_Coerce(i) = 10;
             else
             end
         end
     end
-    save('CellsTrialAveragedAllBADA.mat', 'CellsTrialAveragedAllBADA', 'StimID3', 'StimID3_Coerce');
+    if context_switch == 0
+        save('CellsTrialAveragedAllBADA.mat', 'CellsTrialAveragedAllBADA', 'StimID3', 'StimID3_Coerce');
+    elseif context_switch == 1
+        save('CellsTrialAveragedAllStims.mat', 'CellsTrialAveragedAllStims', 'StimID3', 'StimID3_Coerce');
+    end
 else
-    if no_context_switch == 0
+    if context_switch == 1
         load('CellsTrialAveragedAllStims.mat');
-    elseif no_context_switch == 1
+    elseif context_switch == 0
         load('CellsTrialAveragedAllBADA.mat');
     end
 end
 if plot_switch == 1
-    figure
-    imagesc(CellsTrialAveragedBADA)
-    xlabel('stimuli and trials')
-    ylabel('cells')
-    title([group, 'single trials (CellsTrialAveragedBADA)'])
+    if context_switch == 0
+        figure
+        imagesc(CellsTrialAveragedBADA)
+        xlabel('stimuli and trials')
+        ylabel('cells')
+        title([group, ' single trials (CellsTrialAveragedBADA)'])
+    elseif context_switch == 1
+        figure
+        imagesc(CellsTrialAveragedAllStims)
+        xlabel('stimuli and trials')
+        ylabel('cells')
+        title([group, ' single trials (CellsTrialAveragedAllStims)'])
+    end
 end
 
 %%% End Preprocessing
@@ -179,12 +207,12 @@ if fit_model_switch == 1
     uiwait(gcf);
 end
 
-load('testLinDisc.mat');
-% if no_context_switch == 0
-    yfit = testLinDisc.predictFcn(CellsTrialAveragedAllStims);
-% elseif no_context_switch == 1
-%     yfit = Group12ExemplarsLinDisc.predictFcn(CellsTrialAveragedBADA);
-% end
+load('ExpDataExemplarsQuarterHoldOptBayes.mat');
+if context_switch == 1
+    yfit = ExpDataExemplarsQuarterHoldOptBayes.predictFcn(CellsTrialAveragedAllStims);
+elseif context_switch == 0
+    yfit = Group12ExemplarsLinDisc.predictFcn(CellsTrialAveragedBADA);
+end
 totalfitresults = sum(yfit == StimID3_Coerce) / length(StimID3_Coerce);
 
 confusionmatrixallstims = zeros(30, 3);
@@ -203,7 +231,7 @@ end
 
 figure
 plot(1:10, confusionmatrixallstims((1:10), 3), 'bo-');
-if no_context_switch == 0
+if context_switch == 1
     hold on
     plot(1:10, confusionmatrixallstims((11:20), 3), 'go-');
     hold on
